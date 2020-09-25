1e2ce62a40186f0fc3b39757a791dada98e7cd28
Jean Young Park <jean.park@ssaihq.com>
2020-09-25 15:28:28 -0700

    Quitting Work

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons : [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26}
+    ]
+  }
+
+  switchNameHandler = () => {
+    // Don't do this!  this.state.persons[0].name = 'Maximillion';
+    this.setState({persons: [
+      {name: 'Maximillion', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 27}
+    ]});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="App-title">Hi Im a React App</h1>
+        <p>This is really working!</p>
+        {/* switchNameHandler not switchNameHandler()! no parenth is a reference, with parenth gets called on render*/}
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age ={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age ={this.state.persons[1].age} > My Hobbies: Racing </Person>
+        <Person name={this.state.persons[2].name} age ={this.state.persons[2].age} />
       </div>
     );
+
+    // at least 3 args, at most inifnite args
+    // first argument is element we want to render to the dom
+    // second argument is the configuration for the object
+    // third argument is children, whats nested inside the div 
+      // -- rendered as text default, to use element make React.createElement call
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now'));
   }
 }
 
